442de262725c76429c42d0c61dc448ed958882c2
hmatsu47 <hmatsu47@gmail.com>
2022-06-07 22:32:01 +0900

    Suspense 仮実装・ライブラリ見直し

--- create-readme-app/src/App.tsx
+++ create-readme-app/src/App.tsx
@@ -1,4 +1,4 @@
-  import { Component, ErrorBoundary } from 'solid-js';
+  import { Component, ErrorBoundary, Suspense } from 'solid-js';
   import { Router } from 'solid-app-router';
   import List from './List';
 
@@ -6,9 +6,11 @@ const App: Component = () => {
 
   return (
     <ErrorBoundary fallback={(err) => <div>データ読み込み時にエラーが発生しました：{err.toString()}</div>}>
-      <Router>
-        <List />
-      </Router>
+      <Suspense fallback={<div>読み込み中...</div>}>
+        <Router>
+          <List />
+        </Router>
+      </Suspense>
     </ErrorBoundary>
   );
 };
