c24a604e59b74fc6c68fc82605e90ef226cb72d5
hmatsu47 <hmatsu47@gmail.com>
2022-06-12 19:34:34 +0900

    公開コンテンツ修正

--- puppeteer-app/app.js
+++ puppeteer-app/app.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 
 (async () => {
-  const url_b = 'https://hmatsu47.github.io/create-readme/?route=blog';
+  const url_b = 'https://hmatsu47.github.io/create-readme/?route=articles';
   const url_s = 'https://hmatsu47.github.io/create-readme/?route=slides';
 
   const browser = await puppeteer.launch({
@@ -14,7 +14,7 @@ import puppeteer from 'puppeteer';
   const page = await browser.newPage();
 
   await page.goto(url_b, { waitUntil: 'load' });
-  await page.screenshot({ path: 'production/docs/blog.png' });
+  await page.screenshot({ path: 'production/docs/articles.png' });
   await page.goto(url_s, { waitUntil: 'load' });
   await page.screenshot({ path: 'production/docs/slides.png' });
   await browser.close();
