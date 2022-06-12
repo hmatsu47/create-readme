87211da848485aa5407b9364d34f9771345844aa
hmatsu47 <hmatsu47@gmail.com>
2022-06-12 15:55:22 +0900

    パスルーティング取りやめ

--- create-readme-app/src/List.tsx
+++ create-readme-app/src/List.tsx
@@ -1,19 +1,22 @@
-import { onMount } from 'solid-js';
-import { Route, Routes, useLocation, useNavigate } from 'solid-app-router';
+import { createResource, onMount, Show } from 'solid-js';
+import { useLocation } from 'solid-app-router';
 import Box from '@suid/material/Box';
+import Stack from '@suid/material/Stack';
+import ListParts from './ListParts';
 import Title from './Title';
-import Blog from './Blog';
-import Slides from './Slides';
+import { getApiData } from './apiHandler';
+import { route, setRoute } from './signal';
+import { Feed } from './type';
 
 const List = () => {
-  const routerLocation = useLocation();
-  const navigate = useNavigate();
+  const location = useLocation();
+  const fetchData = async () => await getApiData('./feed.json');
+  const [data] = createResource<Feed | undefined>(fetchData);
 
   onMount (async () => {
-    // query 付きの場合のリダイレクト処理
-    const locationRoute: string = routerLocation.query?.route;
+    const locationRoute: string = location.query?.route;
     if (locationRoute === 'blog' || locationRoute === 'slides') {
-      navigate(`/create-readme/${locationRoute}`, { replace: true });
+      setRoute(locationRoute);
     }
   });
 
@@ -30,20 +33,83 @@ const List = () => {
       aria-live="polite"
     >
       <Title />
-      <Routes>
-        <Route
-          path="/create-readme/"
-          element={<Blog />}
-        />
-        <Route
-          path="/create-readme/blog"
-          element={<Blog />}
-        />
-        <Route
-          path="/create-readme/slides"
-          element={<Slides />}
-        />
-      </Routes>
+      <Stack direction="column">
+        <Show
+          when={route() === 'blog'}
+          fallback={<></>}
+        >
+          <Show
+            when={!data.loading && data()!.qiita! && data()!.qiita!.length > 0}
+            fallback={<></>}
+          >
+            <Box sx={{ paddingTop: "10px" }}></Box>
+            <ListParts
+              title={"Qiita"}
+              id={"qiita"}
+              color={"#55c500"}
+              list={data()!.qiita!}
+              url={"https://qiita.com/hmatsu47"}
+            />
+          </Show>
+          <Show
+            when={!data.loading && data()!.zenn! && data()!.zenn!.length > 0}
+            fallback={<></>}
+          >
+            <Box sx={{ paddingTop: "20px" }}></Box>
+            <ListParts
+              title={"Zenn (Articles & Books)"}
+              id={"zenn"}
+              color={"#3ea8ff"}
+              list={data()!.zenn!}
+              url={"https://zenn.dev/hmatsu47"}
+            />
+          </Show>
+          <Show
+            when={!data.loading && data()!.note! && data()!.note!.length > 0}
+            fallback={<></>}
+          >
+            <Box sx={{ paddingTop: "20px" }}></Box>
+            <ListParts
+              title={"Note"}
+              id={"note"}
+              color={"#2cb696"}
+              list={data()!.note!}
+              url={"https://note.com/hmatsu47"}
+            />
+          </Show>
+          <Show
+            when={!data.loading && data()!.hatena! && data()!.hatena!.length > 0}
+            fallback={<></>}
+          >
+            <Box sx={{ paddingTop: "20px" }}></Box>
+            <ListParts
+              title={"Hatena Blog"}
+              id={"hatena"}
+              color={"#50b5b5"}
+              list={data()!.hatena!}
+              url={"https://hmatsu47.hatenablog.com/"}
+            />
+          </Show>
+        </Show>
+        <Show
+          when={route() === 'slides'}
+          fallback={<></>}
+        >
+          <Show
+            when={!data.loading && data()!.sd! && data()!.sd!.length > 0}
+            fallback={<></>}
+          >
+            <Box sx={{ paddingTop: "10px" }}></Box>
+            <ListParts
+              title={"Speaker Deck"}
+              id={"sd"}
+              color={"#009287"}
+              list={data()!.sd!}
+              url={"https://speakerdeck.com/hmatsu47"}
+            />
+          </Show>
+        </Show>
+      </Stack>
     </Box>
   );
 };
--- create-readme-app/src/Title.tsx
+++ create-readme-app/src/Title.tsx
@@ -1,10 +1,10 @@
-import { Link } from 'solid-app-router';
+
 import Avatar from '@suid/material/Avatar';
 import Box from '@suid/material/Box';
 import Button from '@suid/material/Button';
 import Stack from '@suid/material/Stack';
 import Typography from '@suid/material/Typography';
-import { route } from './signal';
+import { route, setRoute } from './signal';
 
 const Title = () => {
   return (
@@ -33,22 +33,20 @@ const Title = () => {
         >
           hmatsu47 ({route() === 'blog' ? 'Blog Articles' : 'Slides'})
         </Typography>
-        <Link href="/create-readme/blog">
-          <Button
-            color="primary"
-            sx={{ textTransform: "none" }}
-          >
-            Blog Articles
-          </Button>
-        </Link>
-        <Link href="/create-readme/slides">
-          <Button
-            color="primary"
-            sx={{ textTransform: "none" }}
-          >
-            Slides
-          </Button>
-        </Link>
+        <Button
+          onClick={(e) => setRoute('blog')}
+          color="primary"
+          sx={{ textTransform: "none" }}
+        >
+          Blog Articles
+        </Button>
+        <Button
+          onClick={(e) => setRoute('slides')}
+          color="primary"
+          sx={{ textTransform: "none" }}
+        >
+          Slides
+        </Button>
         <Button
           onClick={(e) => window.location.href='https://github.com/hmatsu47'}
           sx={{
