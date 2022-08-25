f069b425dcb41b2b2660f8e215837f45ce3d588c
hmatsu47 <hmatsu47@gmail.com>
2022-08-25 21:28:09 +0900

    RSS フィードの数が足りないときに feed.json の更新を止める

--- tools/feedreader.py
+++ tools/feedreader.py
@@ -4,19 +4,19 @@ import locale
 
 def main():
     locale.setlocale(locale.LC_TIME, 'en_US.UTF-8')
-    # Qiita 記事のフィードを最大 3 件取得
+    # Qiita 記事のフィードを 3 件取得
     qiita = feedparser.parse('https://qiita.com/hmatsu47/feed')
     qiita_dict = convert_feed(qiita, 3)
-    # Zenn 記事のフィードを最大 3 件取得
+    # Zenn 記事のフィードを 3 件取得
     zenn = feedparser.parse('https://zenn.dev/hmatsu47/feed')
     zenn_dict = convert_feed(zenn, 3)
-    # Note 記事のフィードを最大 2 件取得
+    # Note 記事のフィードを 2 件取得
     note = feedparser.parse('https://note.com/hmatsu47/rss')
     note_dict = convert_feed(note, 2)
-    # はてなブログ記事のフィードを最大 2 件取得
+    # はてなブログ記事のフィードを 2 件取得
     hatena = feedparser.parse('https://hmatsu47.hatenablog.com/feed')
     hatena_dict = convert_feed(hatena, 2)
-    # Speaker Deck 資料のフィードを最大 12 件取得
+    # Speaker Deck 資料のフィードを 12 件取得
     sd = feedparser.parse('https://speakerdeck.com/hmatsu47.atom')
     sd_dict = convert_feed(sd, 12)
     # 連結して JSON 出力
@@ -31,6 +31,7 @@ def convert_feed(feed, count):
         dict.append({'link': item['link'], 'title': item['title'], 'published': item['published']})
         i = i + 1
         if (i > (count - 1)): break
+    if (count > i): raise ValueError(f'フィードの件数が不足しています（{i}/{count}）')
     return dict
 
 if __name__ == '__main__':
