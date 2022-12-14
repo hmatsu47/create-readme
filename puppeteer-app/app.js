212b3191f3d0a962ca919d42899b27931941005f
hmatsu47 <hmatsu47@gmail.com>
2022-12-14 19:44:02 +0900

    スクリーンショットが度々失敗するのでスリープ時間を調整

--- puppeteer-app/app.js
+++ puppeteer-app/app.js
@@ -14,7 +14,7 @@ import puppeteer from 'puppeteer';
   const page = await browser.newPage();
 
   await page.goto(url_a, { timeout: 60000, waitUntil: 'load' });
-  new Promise((r) => setTimeout(r, 3000));
+  new Promise((r) => setTimeout(r, 6000));
   await page.screenshot({ path: 'production/docs/articles.png' });
   await page.goto(url_s, { timeout: 60000, waituntil: 'load' });
   new Promise((r) => setTimeout(r, 3000));
